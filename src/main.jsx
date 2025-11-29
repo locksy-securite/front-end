19c5791c99f8b5a240daf8fb1c98145a6bd38ffc
Edric Huang <Edric Huang>
2025-11-29 21:16:37 +0100

    Backend mock implementation

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import setupMock from './mockBackend';
+
+if (import.meta.env.DEV) {
+  setupMock();
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
