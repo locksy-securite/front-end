72b1927e2b413ed571424df65a725210d73b8117
Edric Huang <Edric Huang>
2025-12-01 17:08:04 +0100

    Update AccountDropdown.jsx

--- src/components/dashboard/AccountDropdown.jsx
+++ src/components/dashboard/AccountDropdown.jsx
@@ -8,11 +8,15 @@ export default function AccountDropdown({ email, onLogout }) {
                 aria-label="Menu compte utilisateur"
                 className="btn btn-circle btn-secondary"
             >
-                <img
-                    src="/compte.svg"
-                    alt="Icône compte utilisateur"
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
                     className="w-5 h-5"
-                />
+                    fill="currentColor"
+                >
+                    <path d="M12,12A6,6,0,1,0,6,6,6.006,6.006,0,0,0,12,12ZM12,2A4,4,0,1,1,8,6,4,4,0,0,1,12,2Z" />
+                    <path d="M12,14a9.01,9.01,0,0,0-9,9,1,1,0,0,0,2,0,7,7,0,0,1,14,0,1,1,0,0,0,2,0A9.01,9.01,0,0,0,12,14Z" />
+                </svg>
             </div>
 
             {/* Contenu du dropdown */}
